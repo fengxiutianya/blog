e543e74a3970f904927f6189aaf9162eb06bd63c
zhangke <398757724@qq.com>
2019-01-08 06:08:38 +0000

    测试docker

--- upload.sh
+++ upload.sh
@@ -2,6 +2,8 @@
 if [ ! -n "$1" ] ;then
     echo "请输入此次提交git的注释"
 else
+    git config  user.email "398757724@qq.com"
+    git config  user.name "zhangke"
     hexo clean
     git add --all
     git commit -m $1
