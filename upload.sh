1a50f4dea8a95c7e0b2f376ecc2da721e8ad0299
zhangke <398757724@qq.com>
2019-04-16 23:17:43 +0800

    添加numpy之axis文章

--- upload.sh
+++ upload.sh
@@ -2,8 +2,8 @@
 if [ ! -n "$1" ] ;then
     echo "请输入此次提交git的注释"
 else
-    git config  user.email "398757724@qq.com"
-    git config  user.name "zhangke"
+    git config --global user.email "398757724@qq.com"
+    git config  --global user.name "zhangke"
     hexo clean
     git add --all
     git commit -m $1
